aee2fb5a507557192ded6e92ccbce6bfbf483a7a
Anthony Winters <a.r.winters@nforce-it.nl>
2019-02-06 02:51:45 +0100

    Chapter 7 part1

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
     url(r'^page/(?P<page_id>\d+)/$', views.page, name='page'),
-
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,8 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+from django.views.decorators.csrf import csrf_protect
 
 # def index(request):
 #     return HttpResponse("Tony says hello! <p><a href='/rango/about'>About Page</a>")
@@ -87,3 +89,33 @@ def page(request, page_id):
     context_dict = {'pages': page_list}
 
     return render_to_response('rango/page.html', context_dict, context)
+
+
+# @csrf_protect
+def add_category(request):
+    # Get the context from the request.
+    # context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    #return render_to_response('rango/add_category.html', {'form': form}, context)
+    return render(request, 'rango/add_category.html', {'form': form})
